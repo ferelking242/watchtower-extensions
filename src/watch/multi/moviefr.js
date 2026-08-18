4dfd6d5b5d02488f8aad217052ff42973e3bd6d7
ferelking242 <ferelking39@gmail.com>
2026-08-18 16:34:33 +0000

    fix(moviefr): bump version to 1.0.4 and fix 9anime icon 404

--- src/watch/multi/moviefr.js
+++ src/watch/multi/moviefr.js
@@ -7,7 +7,7 @@ const watchtowerSources = [{
     "typeSource": "single",
     "isManga": false,
     "itemType": 1,
-    "version": "1.0.3",
+    "version": "1.0.4",
     "dateFormat": "",
     "dateFormatLocale": "",
     "isNsfw": false,
